e035a0729622301ea1cbae5ad68cf49b7d6a34c9
JarrettTo <justin.jarrett.to@gmail.com>
2023-04-17 23:06:12 +0800

    initial commit

---
+++ client/src/action/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const API = axios.create({ baseURL: "https://imdbapi-production.up.railway.app/"}); //creates base connection with API
+API.interceptors.request.use((req) => {
+
+  return req;
+});
+
+export const insert = (post) => API.post(`/database/insert`,post);
+export const update = (post) => API.post(`/database/update`,post);
+export const delete1 = (id, post) => API.post(`/database/delete/${id}`,post);
+export const viewall= (id) => API.get(`/database/viewall/${id}`);
+export const view= (id,post) => API.post(`/database/view/${id}`, post);
+export const setCommited= () => API.get(`/database/commited`);
+export const setUncommited= () => API.get(`/database/uncommited`);
+export const setRepeatable= () => API.get(`/database/repeatable`);
+export const setSerializable= () => API.get(`/database/serializable`);
